f9fd7d31072b23570c55915e13d0c75f7e295391
tevin davis <aod.td1105@gmail.com>
2020-08-13 21:29:25 -0400

    Able to add items to players inventory. Still working on removing items

--- src/adv.py
+++ src/adv.py
@@ -1,39 +1,54 @@
 # from room import Room
 # from player import Player
 class Player:
-    def __init__(self, name, room):
+    def __init__(self, name, room, items=[]):
         self.name = name
         self.room = room
+        self.items = items
 
     def __str__(self):
         return f'{self.name} is currently in the {self.room}'
+    
+    def add_item(self, item):
+        self.items.append(item)
+    
+    def remove_item(self, index):
+        print(f'Chose item to remove: {self.items}')
+        self.items.pop(index)
+    
+    def view_items(self):
+        print(f'Current items: {self.items}')
 
 class Room:
-    def __init__(self, name, description):
-        self.name = name,
+    def __init__(self, name, description, treasure=[]):
+        self.name = name
         self.description = description
+        self.treasure = treasure
 
 # Declare all the rooms
 
 # Declare all the rooms
+treasure_room_items = ['1. jewels', '2. gold']
+overlook_items = ['1. shield', '2. armor']
+foyer_items = ['1. sword', '2. elixir']
 
 room = {
     'outside': Room("Outside Cave Entrance",
                     "North of you, the cave mount beckons"),
 
     'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", foyer_items),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", overlook_items),
 
     'narrow': Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", treasure_room_items),
 }
 
 # Link rooms together
@@ -82,19 +97,27 @@ while True:
 # * Prints the current description (the textwrap module might be useful here).
         print(f'{tevin.room.description}')
 # * Waits for user input and decides what to do.
-    value = input('Please choose one of these directions (n, s, w, e or q to quit):\n')
-    if value in {'n', 's', 'w', 'e', 'q'}:
+    value = input('Please choose one of these directions (n, s, w, e, items, remove or q to quit):\n')
+    if value in {'n', 's', 'w', 'e', 'items', 'q'}:
         if value == 'n':
             if tevin.room.n_to is not None:
                 tevin.room = tevin.room.n_to
                 print(f'You have entered {tevin.room.name}')
+                if tevin.room.treasure:              
+                    print(f'You have found: {tevin.room.treasure}')
+                    chose_item = input('Chose an item to pick up or c to continue\n')
+                    tevin.add_item(tevin.room.treasure[int(chose_item)-1])
             else:
                 print('This is a dead end')
 
         elif value == 's':
             if tevin.room.s_to is not None:
                 tevin.room = tevin.room.s_to
-                print(f'You have entered {tevin.room.name}')   
+                print(f'You have entered {tevin.room.name}')
+                if tevin.room.treasure:
+                    print(f'You have found: {tevin.room.treasure}')
+                    chose_item = input('Chose an item to pick up or c to continue\n')
+                    tevin.add_item(tevin.room.treasure[int(chose_item)-1])   
             else:
                 print('This is a dead end')                  
             
@@ -102,6 +125,10 @@ while True:
             if tevin.room.w_to is not None:
                 tevin.room = tevin.room.w_to
                 print(f'You have entered {tevin.room.name}')
+                if tevin.room.treasure:
+                    print(f'You have found: {tevin.room.treasure}')
+                    chose_item = input('Chose an item to pick up or c to continue\n')
+                    tevin.add_item(tevin.room.treasure[int(chose_item)-1])
             else:
                 print('This is a dead end')
 
@@ -109,8 +136,19 @@ while True:
             if tevin.room.s_to is not None:
                 tevin.room = tevin.room.e_to
                 print(f'You have entered {tevin.room.name}')
+                if tevin.room.treasure:
+                    print(f'You have found: {tevin.room.treasure}')
+                    chose_item = input('Chose an item to pick up or c to continue\n')
+                    tevin.add_item(tevin.room.treasure[int(chose_item)-1])
             else:
                 print('This is a dead end')
+        
+        elif value == 'items':
+            tevin.view_items()
+        
+        elif value == 'remove':
+            remove_item = input('Chose item to remove\n')
+            tevin.remove_item(int(remove_item))
 
         elif value == 'q':
             print(f'{tevin.name}, thank you for playing!')
--- src/player.py
+++ src/player.py
@@ -1,10 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, room):
+    def __init__(self, name, items=[], room):
         self.name = name
+        self.items = items
         self.room = room
 
     def __str__(self):
         return f'{self.name} is currently in the {self.room}'
     
+    def add_item(self, item):
+        self.items.append(item)
+    
+    def remove_item(self, index):
+        self.items.pop(index)
+    
+    def view_items(self):
+        print(self.items)
